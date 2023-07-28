6025ef6f3a63c02eadeb5717a1dec9e3b39f3406
Atta ur Rehman <attaurr786@outlook.com>
2023-07-28 17:10:43 +0500

    SearchBar done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+   <div>
+    <div className='main mt-5 flex justify-center'>
+<input className='bg-gray-100 placeholder-gray-500 outline-none px-7 py-1 border-rounded-sm' type='text' placeholder='Search Here'>
+</input>
+<button className='bg-black text-white px-4 py-1 rounded-sm'>
+    Search
+</button>
+    </div>
+   </div>
   )
 }
 
