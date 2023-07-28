9fbec4582d44886992135f427684465846c94c5d
Atta ur Rehman <attaurr786@outlook.com>
2023-07-28 17:28:24 +0500

    movie cards done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const MovieCard = () => {
   return (
-    <div>MovieCard</div>
+    <div>
+        <div>
+            <div className='flex flex-wrap px  lg:px-4'>
+                <div className='bg-black p-4 m-5 lg:w-1/5 w-full rounded-2xl'>
+                    <img className='w-full pb-2 rounded-2xl' src='https://dummyimage.com/720x400'></img>
+                    <h2 className='text-white font-bold'>Movie Title</h2>
+                    <h6 className='text-white'>Description</h6>
+                </div>
+                <div className='bg-black p-4 m-5 lg:w-1/5 w-full rounded-2xl'>
+                    <img className='w-full pb-2 rounded-2xl' src='https://dummyimage.com/720x400'></img>
+                    <h2 className='text-white font-bold'>Movie Title</h2>
+                    <h6 className='text-white'>Description</h6>
+                </div>
+                <div className='bg-black p-4 m-5 lg:w-1/5 w-full rounded-2xl'>
+                    <img className='w-full pb-2 rounded-2xl' src='https://dummyimage.com/720x400'></img>
+                    <h2 className='text-white font-bold'>Movie Title</h2>
+                    <h6 className='text-white'>Description</h6>
+                </div>
+                <div className='bg-black p-4 m-5 lg:w-1/5 w-full rounded-2xl'>
+                    <img className='w-full pb-2 rounded-2xl' src='https://dummyimage.com/720x400'></img>
+                    <h2 className='text-white font-bold'>Movie Title</h2>
+                    <h6 className='text-white'>Description</h6>
+                </div>
+                <div className='bg-black p-4 m-5 lg:w-1/5 w-full rounded-2xl'>
+                    <img className='w-full pb-2 rounded-2xl' src='https://dummyimage.com/720x400'></img>
+                    <h2 className='text-white font-bold'>Movie Title</h2>
+                    <h6 className='text-white'>Description</h6>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
