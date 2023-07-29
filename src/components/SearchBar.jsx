f4e6935ae950943b4cd74a5126905562b36dfca2
Atta ur Rehman <attaurr786@outlook.com>
2023-07-29 12:48:41 +0500

    fetching code start

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-const SearchBar = () => {
+function SearchBar ({SearchBar, setSearchMovie,fetchMovieData}) {
   return (
    <div>
     <div className='main mt-5 flex justify-center'>
 <input className='bg-gray-100 placeholder-gray-500 outline-none px-7 py-1 border-rounded-sm' type='text' placeholder='Search Here'>
+value={setSearchMovie}
+onchange={(e) => setSearchMovie(e.target.value)}
 </input>
-<button className='bg-black text-white px-4 py-1 rounded-sm'>
+<button 
+onClick={fetchMovieData}
+className='bg-black text-white px-4 py-1 rounded-sm'>
     Search
 </button>
     </div>
